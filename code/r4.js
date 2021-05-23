2ab45ea86b5c6b022283f8efca47a721b7621ce4
feijooso <sfeijoo@fi.uba.ar>
2021-05-23 18:03:25 -0300

    changed api server

--- code/r4.js
+++ code/r4.js
@@ -1,23 +1,22 @@
-function CallPageMethod(methodName, query) {
-    var loc = "http://api.weatherstack.com";
-    loc = (loc.substr(loc.length - 1, 1) == "/") ? loc + "default.aspx" : loc;
+function CallPageMethod(query) {
+    var loc = "http://api.openweathermap.org/data/2.5";
     $.ajax({
         type: "GET",
-        url: loc + "/" + methodName,
+        url: loc + "/" + "weather",
         cache: false,
         data: {
-            access_key: '347ecee25ef29efa0cd06cd0da0d32dd',
-            query: query
+           q: query,
+           appid: '902f718fe489465ff24fc7dc39cabc82'
         },
         dataType: "json",
     })
 
     .done(function(data) {
         console.log(data);
-        var tag = "#" + data.location.name;
+        var tag = "#" + data.name;
         resultado = document.querySelector(tag);
-        resultado.innerHTML = data.current.temperature;
-        return(data.current.temperature);
+        resultado.innerHTML = data.main.temp;
+        return(data.main.temp);
 
     })
 
@@ -26,9 +25,9 @@ function CallPageMethod(methodName, query) {
     });
 }
 
-function CallPageMethods(methodName) {
-	CallPageMethod(methodName, "Rome");
-	CallPageMethod(methodName, "London");
-	CallPageMethod(methodName, "Madrid");
-	CallPageMethod(methodName, "Mumbai");
+function CallPageMethods() {
+	CallPageMethod("Rome");
+	CallPageMethod("London");
+	CallPageMethod("Madrid");
+	CallPageMethod("Mumbai");
 }
